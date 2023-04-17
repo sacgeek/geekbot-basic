de07dec333417f6f4d78215db6f01844c97972d4
sacgeek <43549331+sacgeek@users.noreply.github.com>
2023-04-16 22:43:45 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,21 +55,21 @@ intents.messages = True
 intents.emojis = True
 
 
-client = discord.Client(intents=intents)
+#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='$', intents=intents)
-# bot commands aren't working:
-# @bot.command(name='ping')
-# async def ping(ctx):
-#     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
-#
-#
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+
 @bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
 ####################################################
 # client commands are working:
 # Client on_ready connect to Discord:
-@client.event
+@bot.event
 async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
@@ -78,13 +78,15 @@ async def on_ready():
     print(f'{guild.name}(id: {guild.id})')
 
 # Very basic hi/hello on_message event:
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    # if message.author == client.user:
+    #     return
+    #
+    # if message.content.startswith('hi'):
+    #     await message.channel.send('Hello!')
 
-    if message.content.startswith('hi'):
-        await message.channel.send('Hello!')
+    await bot.process_commands(message)
 
 @bot.command()
 async def react(ctx):
@@ -103,4 +105,4 @@ async def react(ctx):
     #     await message.channel.send(response)
 
 # run the bot:
-client.run(TOKEN)
+bot.run(TOKEN)
